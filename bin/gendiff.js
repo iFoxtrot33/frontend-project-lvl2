d4979267b1ea512ada09868b07042d7568e5c72d
iurii <furmaniurii@gmail.com>
2022-06-29 15:26:19 +0300

    Update gendiff
    Add format feature to the help menu

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-import { Command } from "./../node_modules/commander/esm.mjs";
-const program = new Command();
+import { program } from "./../node_modules/commander/esm.mjs";
+
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-program.parse();
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+// eslint-disable-next-line no-undef
+program.parse(process.argv);
  
 // eslint-disable-next-line no-undef
 console.log(program);
