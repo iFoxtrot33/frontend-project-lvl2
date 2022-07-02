1aced5674d30cf0559757dd9f4531e5f37b1e077
iurii <furmaniurii@gmail.com>
2022-07-03 00:11:23 +0300

    Update path options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import { program } from "./../node_modules/commander/esm.mjs";
 import fs from 'fs';
+import findDiff from "./../index.js"
+const gendiff = (path1, path2) => findDiff(path1.toString(), path2.toString());
+
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
@@ -11,5 +14,4 @@ program
 program.parse(process.argv);
  
 // eslint-disable-next-line no-undef
-const file1 = fs.readFileSync('./fixtures/file1.json')
-console.log(file1);
+
--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 import fs from 'fs';
 import path from 'path';
+import process from 'process'
 import keys from './node_modules/lodash/keys.js'
 import union from './node_modules/lodash/union.js'
 
-const getFixturePath = (filename) => path.join('fixtures', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (p) => path.resolve(process.cwd(), p).trim();
+const readFile = (path) => fs.readFileSync(getFixturePath(path), 'utf-8');
 
-const file1 = JSON.parse(readFile('file1.json'));
-const file2 = JSON.parse(readFile('file2.json'));
+const file = (path) => JSON.parse(readFile(path));
 
-const findDiff = (file1, file2) => {
+const findDiff = (path1, path2) => {
+  const file1 =  file(path1);
+  const file2 =  file(path2);
   const key1 = keys(file1);
   const key2 = keys(file2);
   const commonArr = union(key1, key2);
   const temp = [];
-  const findDiff = commonArr.filter((key) => {
+  const findD = commonArr.filter((key) => {
     const one = file1[key];
     const two = file2[key];
     if (one === two) {
       temp.push(`  ${key}:${two}`);
       return temp;
+    }
+    if ((one !== two) && ( one !== undefined) && ( two !== undefined) ) {
+        temp.push(`- ${key}:${one}`);
+        temp.push(`+ ${key}:${two}`);
+        return temp;
     }
     if ((one !== two) && ( one !== undefined)) {
         temp.push(`- ${key}:${one}`);
@@ -29,15 +36,14 @@ const findDiff = (file1, file2) => {
         temp.push(`+ ${key}:${two}`);
         return temp;
     }
-  });
-  const sortedResult = temp.sort((a, b) => a.localeCompare(b))
+  }, {});
+  const sortedResult = temp.sort((a,b) => a.charCodeAt(2)-b.charCodeAt(2))
   const final = {};
   const makeObj = sortedResult.map((element) => {
     const t = element.split(':');
     final[t[0]] = t[1];
   });
-  console.log(temp);
   return final;
 };
 
-console.log(findDiff(file1, file2));
+export default findDiff;
