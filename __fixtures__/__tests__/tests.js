6c5a5feedd9b0d5cee79b55f74134b1bfb0b0b70
iurii <furmaniurii@gmail.com>
2022-07-08 16:00:53 +0300

    Update tests

--- __fixtures__/__tests__/tests.js
+++ __fixtures__/__tests__/tests.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@jest/globals';
 import { findDiff  } from "./../../index.js"
 
-test('Initial test', () => {
+test('1st test - json & json', () => {
 expect(findDiff('__fixtures__/file1.json', '__fixtures__/file2.json' )).toEqual({
 '- follow': 'false',
 '  host': 'hexlet.io',
@@ -10,3 +10,53 @@ expect(findDiff('__fixtures__/file1.json', '__fixtures__/file2.json' )).toEqual(
 '+ timeout': '20',
 '+ verbose': 'true'});
 });
+
+test('2nd test - yaml & yaml ', () => {
+    expect(findDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml' )).toEqual({
+    '- follow': 'false',
+    '  host': 'hexlet.io',
+    '- proxy': '123.234.53.22',
+    '- timeout': '50',
+    '+ timeout': '20',
+    '+ verbose': 'true'});
+    });
+
+test('3rd test - yml & yml', () => {
+    expect(findDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml' )).toEqual({
+    '- follow': 'false',
+    '  host': 'hexlet.io',
+    '- proxy': '123.234.53.22',
+    '- timeout': '50',
+    '+ timeout': '20',
+    '+ verbose': 'true'});
+    });
+
+test('4th test - json & yaml', () => {
+    expect(findDiff('__fixtures__/file1.json', '__fixtures__/file2.yaml' )).toEqual({
+    '- follow': 'false',
+    '  host': 'hexlet.io',
+    '- proxy': '123.234.53.22',
+    '- timeout': '50',
+    '+ timeout': '20',
+    '+ verbose': 'true'});
+    });
+
+test('5th test - json & yml', () => {
+    expect(findDiff('__fixtures__/file1.json', '__fixtures__/file2.yml' )).toEqual({
+    '- follow': 'false',
+    '  host': 'hexlet.io',
+    '- proxy': '123.234.53.22',
+    '- timeout': '50',
+    '+ timeout': '20',
+    '+ verbose': 'true'});
+    });
+
+test('6th test - yaml & yml', () => {
+    expect(findDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yml' )).toEqual({
+    '- follow': 'false',
+    '  host': 'hexlet.io',
+    '- proxy': '123.234.53.22',
+    '- timeout': '50',
+    '+ timeout': '20',
+    '+ verbose': 'true'});
+    });
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'process'
-import keys from './node_modules/lodash/keys.js'
-import union from './node_modules/lodash/union.js'
+import keys from './node_modules/lodash/keys.js';
+import union from './node_modules/lodash/union.js';
+import last from './node_modules/lodash/last.js';
+import yaml from 'js-yaml';
+
+
 
 const getFixturePath = (p) => path.resolve(process.cwd(), p).trim();
 const readFile = (path) => fs.readFileSync(getFixturePath(path), 'utf-8');
 
-const file = (path) => JSON.parse(readFile(path));
+const parseFile = (filePath) => {
+  const fileExtension = last(filePath.split('.')).toLowerCase();
+  if (fileExtension === 'json') {
+    return JSON.parse(readFile(filePath));
+  }
+  if (fileExtension === 'yaml' || fileExtension === 'yml') {
+    return yaml.load(readFile(filePath));
+  }
+  throw new Error('Unknown file format. Please use json, yaml or yml files.');
+};
 
 export const findDiff = (filepath1, filepath2) => {
-  const file1 =  file(filepath1);
-  const file2 =  file(filepath2);
+  const file1 =  parseFile(filepath1);
+  const file2 =  parseFile(filepath2);
   const key1 = keys(file1);
   const key2 = keys(file2);
   const commonArr = union(key1, key2);
