cd4a11ece4657f76009e9025ebdc08e4a8780607
iurii <furmaniurii@gmail.com>
2022-07-17 09:46:31 +0300

    Update tests

--- __fixtures__/__tests__/tests.js
+++ __fixtures__/__tests__/tests.js
@@ -2,9 +2,21 @@ import { test, expect } from '@jest/globals';
 import  findDiff   from "./../../src/index.js"
 import { parseFile } from "./../../src/parser.js"
 import { formatter} from "./../../src/stylish.js"
+import { formatPlain } from "./../../src/formatters/plain.js"
+
+const plainObject = 
+`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`
+
 
 test('1st test - json & json', () => {
-expect(formatter(findDiff(parseFile('./__fixtures__/file1.json'), parseFile('./__fixtures__/file2.json' )))).toEqual(   `{
+expect(formatter(findDiff(parseFile('./__fixtures__/file1.json'), parseFile('./__fixtures__/file2.json' )))).toEqual( `{
     common: {
       + follow: false
         setting1: Value 1
@@ -51,81 +63,39 @@ expect(formatter(findDiff(parseFile('./__fixtures__/file1.json'), parseFile('./_
 });
 
 test('2nd test - yaml & yaml ', () => {
-    expect(formatter(findDiff(parseFile('./__fixtures__/file1.yaml'), parseFile('./__fixtures__/file2.yaml' )))).toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+    expect(formatter(findDiff(parseFile('./__fixtures__/file1.yaml'), parseFile('./__fixtures__/file2.yaml' )))).toEqual(plainObject);
     });
 
 test('2rd test - yml & yml ', () => {
-    expect(formatter(findDiff(parseFile('./__fixtures__/file1.yml'), parseFile('./__fixtures__/file2.yml' )))).toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+    expect(formatter(findDiff(parseFile('./__fixtures__/file1.yml'), parseFile('./__fixtures__/file2.yml' )))).toEqual(plainObject);
     });
 
 
 test('4th test - yaml & yml ', () => {
-    expect(formatter(findDiff(parseFile('./__fixtures__/file1.yaml'), parseFile('./__fixtures__/file2.yml' )))).toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+    expect(formatter(findDiff(parseFile('./__fixtures__/file1.yaml'), parseFile('./__fixtures__/file2.yml' )))).toEqual(plainObject);
     });
 
-    `{
-        common: {
-          + follow: false
-            setting1: Value 1
-          - setting2: 200
-          - setting3: true
-          + setting3: null
-          + setting4: blah blah
-          + setting5: {
-                key5: value5
-            }
-            setting6: {
-                doge: {
-                  - wow: 
-                  + wow: so much
-                }
-                key: value
-              + ops: vops
-            }
-        }
-        group1: {
-          - baz: bas
-          + baz: bars
-            foo: bar
-          - nest: {
-                key: value
-            }
-          + nest: str
-        }
-      - group2: {
-            abc: 12345
-            deep: {
-                id: 45
-            }
-        }
-      + group3: {
-            deep: {
-                id: {
-                    number: 45
-                }
-            }
-            fee: 100500
-        }
-    }`
+test('5th test - json & yaml ', () => {
+    expect(formatter(findDiff(parseFile('./__fixtures__/file3.json'), parseFile('./__fixtures__/file2.yaml' )))).toEqual(plainObject);
+    });
 
+test('6th test - json & yml ', () => {
+    expect(formatter(findDiff(parseFile('./__fixtures__/file3.json'), parseFile('./__fixtures__/file2.yml' )))).toEqual(plainObject);
+    });     
+
+test('7th test - json & json / plain format', () => {
+    const result =
+`Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]
+`
+    expect(formatPlain(findDiff(parseFile('./__fixtures__/file1.json'), parseFile('./__fixtures__/file2.json' )))).toEqual(result);
+});
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,6 +4,9 @@ const isComplex = (val) => {
     if (_.isPlainObject(val) === true) {
         return '[complex value]';
     }
+    if ((val === true) || (val === false) || (val === null)) {
+        return val;
+    }
     return `'${val}'`;
 };
 
@@ -14,9 +17,9 @@ export const formatPlain = (obj) => {
         case 'add':
             return `Property '${path}' was added with value: ${isComplex(node.val)}\n`;
         case 'remove':
-            return `Property '${node.key}' was removed\n`;
+            return `Property '${path}' was removed\n`;
         case 'updated':
-            return `Property '${node.key}' was updated. From ${isComplex(node.val1)} to ${isComplex(node.val2)}\n`;
+            return `Property '${path}' was updated. From ${isComplex(node.val1)} to ${isComplex(node.val2)}\n`;
         case 'recursion':
             return `${iter(node.children, [path]).join('')}`;
     }
