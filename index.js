5a3f710bcbdfd14035ad4f93677c2b1c2812fd40
iurii <furmaniurii@gmail.com>
2022-07-19 10:44:26 +0300

    Fix tests 11

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { program } from "./node_modules/commander/esm.mjs"
+import { program } from "./node_modules/commander/index.js"
 import  findDiff   from "./src/index.js"
 import { parseFile } from "./src/parser.js"
 import { formatter} from "./src/stylish.js"
