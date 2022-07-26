84ce5f011df1c2877488790f5b016dd35e0ae4bb
iurii <furmaniurii@gmail.com>
2022-07-26 06:11:15 +0300

    Fix corrections 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,6 @@ program
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-  // eslint-disable-next-line no-undef
     console.log(findDifference(filepath1, filepath2, program.opts().format));
   });
 program.parse();
